6ebf745b3031cf86797958da974e47bf2c371790
Silvia Ehrmann <Silvia Ehrmann>
2015-10-28 12:38:50 +0100

    Fibonacci Iteration

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,22 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	int N,fib=0,fib1=1,i;
+	
+	cout << "N=";
+	cin>>N;
+	
+	for(i=1;i<=N;i++)
+	{
+		fib+=fib1;
+		fib1=fib-fib1;
+		
+		cout <<fib<<"\n";
+	}
+	cout<<"Die "<<N<<".te Fibonacci-Zahl ist "<<fib<<endl;
+	
+	return 0;
+}
